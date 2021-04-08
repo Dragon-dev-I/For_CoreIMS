d5d29d4f3347203b9ea6e86e3e38e9663f087a97
Иван Разинков <razinckov.vanya@mail.ru>
2021-04-08 12:05:13 +0300

    var a = new Dollar { Val = 1 }; var b = new Ruble { Val = 30 }; Console.WriteLine(a + b);

--- MoneyOperations/Money.cs
+++ MoneyOperations/Money.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MoneyOperations
 {
-    class Money
+    public abstract class Money
     {
         private int val = 0;
 
@@ -14,6 +14,34 @@ namespace MoneyOperations
         {
             get { return val; }
             set { val = value; }
+        }
+
+        public abstract double ExchangeRate();
+
+
+        public double AbsoluteValue(double exchangeRate)
+        {
+            return exchangeRate * Val;
+        }
+
+        public static double GetExchangeRate(Money money)
+        {
+            return money.ExchangeRate();
+        }
+
+        public static double REAbsoluteValue(double val, double changeRate)
+        {
+            return val / changeRate;
+        }
+
+        public static double operator +(Money first, Money second)
+        {
+            if (first.GetType() == second.GetType())
+                return first.val + second.val;
+
+            var exchangeRate_first = GetExchangeRate(first);
+            var exchangeRate_second = GetExchangeRate(second);
+            return REAbsoluteValue(first.AbsoluteValue(exchangeRate_first) + second.AbsoluteValue(exchangeRate_second), exchangeRate_first);
         }
     }
 }
--- MoneyOperations/Program.cs
+++ MoneyOperations/Program.cs
@@ -9,253 +9,34 @@ namespace MoneyOperations
     class Dollar : Money
     {
         private const int exchange_rate = 60;
-        public int Dollar_exchange_rate
+        public override double ExchangeRate()
         {
-            get { return exchange_rate; }
-        }
-        public int AbsoluteValue()
-        {
-            return exchange_rate * Val;
-        }
-        public double REAbsoluteValue(int val)
-        {
-            return (double)val / (double)exchange_rate;
-        }
-        public static String operator +(Dollar c1, Euro c2)
-        {
-            Dollar res = new Dollar();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "$";
-        }
-        public static String operator +(Dollar c1, Ruble c2)
-        {
-            Dollar res = new Dollar();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "$";
-        }
-        public static String operator -(Dollar c1, Euro c2)
-        {
-            Dollar res = new Dollar();
-            return res.REAbsoluteValue(c1.AbsoluteValue() - c2.AbsoluteValue()).ToString() + "$";
-        }
-        public static String operator -(Dollar c1, Ruble c2)
-        {
-            Dollar res = new Dollar();
-            return res.REAbsoluteValue(c1.AbsoluteValue() - c2.AbsoluteValue()).ToString() + "$";
-        }
-        public static bool operator >(Dollar c1, Euro c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator >(Dollar c1, Ruble c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator <(Dollar c1, Euro c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
-        }
-        public static bool operator <(Dollar c1, Ruble c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
+            return exchange_rate;
         }
     }
     class Euro : Money
     {
         private const int exchange_rate = 90;
-        public int Euro_exchange_rate
+        public override double ExchangeRate()
         {
-            get { return exchange_rate; }
-        }
-        public int AbsoluteValue()
-        {
-            return exchange_rate * Val;
-        }
-        public double REAbsoluteValue(int val)
-        {
-            return (double)val / (double)exchange_rate;
-        }
-        public static String operator +(Euro c1, Dollar c2)
-        {
-            Euro res = new Euro();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "e";
-        }
-        public static String operator +(Euro c1, Ruble c2)
-        {
-            Euro res = new Euro();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "e";
-        }
-        public static String operator -(Euro c1, Dollar c2)
-        {
-            Euro res = new Euro();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "e";
-        }
-        public static String operator -(Euro c1, Ruble c2)
-        {
-            Euro res = new Euro();
-            return res.REAbsoluteValue(c1.AbsoluteValue() - c2.AbsoluteValue()).ToString() + "e";
-        }
-        public static bool operator >(Euro c1, Dollar c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator >(Euro c1, Ruble c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator <(Euro c1, Dollar c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
-        }
-        public static bool operator <(Euro c1, Ruble c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
+            return exchange_rate;
         }
     }
     class Ruble : Money
     {
         private const int exchange_rate = 1;
-        public int Ruble_exchange_rate
+        public override double ExchangeRate()
         {
-            get { return exchange_rate; }
-        }
-        public int AbsoluteValue()
-        {
-            return exchange_rate * Val;
-        }
-        public double REAbsoluteValue(int val)
-        {
-            return (double)val / (double)exchange_rate;
-        }
-        public static String operator +(Ruble c1, Euro c2)
-        {
-            Ruble res = new Ruble();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "r";
-        }
-        public static String operator +(Ruble c1, Dollar c2)
-        {
-            Ruble res = new Ruble();
-            return res.REAbsoluteValue(c1.AbsoluteValue() + c2.AbsoluteValue()).ToString() + "r";
-        }
-        public static String operator -(Ruble c1, Euro c2)
-        {
-            Ruble res = new Ruble();
-            return res.REAbsoluteValue(c1.AbsoluteValue() - c2.AbsoluteValue()).ToString() + "r";
-        }
-        public static String operator -(Ruble c1, Dollar c2)
-        {
-            Ruble res = new Ruble();
-            return res.REAbsoluteValue(c1.AbsoluteValue() - c2.AbsoluteValue()).ToString() + "r";
-        }
-        public static bool operator >(Ruble c1, Dollar c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator >(Ruble c1, Euro c2)
-        {
-            return c1.AbsoluteValue() > c2.AbsoluteValue();
-        }
-        public static bool operator <(Ruble c1, Dollar c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
-        }
-        public static bool operator <(Ruble c1, Euro c2)
-        {
-            return c1.AbsoluteValue() < c2.AbsoluteValue();
+            return exchange_rate;
         }
     }
     class Program
     {
-
         static void Main(string[] args)
         {
-            String x_val = "";
-            String y_val = "";
-
-            String x_currency = "";
-            String y_currency = "";
-
-            String operation = "";
-
-            String mes_number = "Enter a number";
-            String mes = "Enter currency sign (r - ruble, $ - dollar, e - euro)";
-            String mes_operation = "Enter operation (> comparison, < comparison, + summation, - subtraction)";
-
-            Console.WriteLine(mes_number);
-            x_val = Console.ReadLine();
-            Console.WriteLine(mes);
-            x_currency = Console.ReadLine();
-
-            Console.WriteLine(mes_number);
-            y_val = Console.ReadLine();
-            Console.WriteLine(mes);
-            y_currency = Console.ReadLine();
-
-            Console.WriteLine(mes_operation);
-            operation = Console.ReadLine();
-
-
-            dynamic money_val1 = 0;
-            dynamic money_val2 = 0;
-
-            switch (x_currency)
-            {
-                case "r":
-                    Ruble ruble = new Ruble();
-                    ruble.Val = Convert.ToInt32(x_val);
-                    money_val1 = ruble;
-                    break;
-                case "$":
-                    Dollar dollar = new Dollar();
-                    dollar.Val = Convert.ToInt32(x_val);
-                    money_val1 = dollar;
-                    break;
-                case "e":
-                    Euro euro = new Euro();
-                    euro.Val = Convert.ToInt32(x_val);
-                    money_val1 = euro;
-                    break;
-                default:
-                    Console.WriteLine("Error: operation (> comparison, < comparison, + summation, - subtraction) not found");
-                    break;
-            }
-            switch (y_currency)
-            {
-                case "r":
-                    Ruble ruble = new Ruble();
-                    ruble.Val = Convert.ToInt32(y_val);
-                    money_val2 = ruble;
-                    break;
-                case "$":
-                    Dollar dollar = new Dollar();
-                    dollar.Val = Convert.ToInt32(y_val);
-                    money_val2 = dollar;
-                    break;
-                case "e":
-                    Euro euro = new Euro();
-                    euro.Val = Convert.ToInt32(y_val);
-                    money_val2 = euro;
-                    break;
-                default:
-                    Console.WriteLine("Error: operation (> comparison, < comparison, + summation, - subtraction) not found");
-                    break;
-            }
-            switch (operation)
-            {
-                case ">":
-                    Console.WriteLine(money_val1 > money_val2);
-                    break;
-                case "<":
-                    Console.WriteLine(money_val1 < money_val2);
-                    break;
-                case "+":
-                    Console.WriteLine(money_val1 + money_val2);
-                    break;
-                case "-":
-                    Console.WriteLine(money_val1 - money_val2);
-                    break;
-                default:
-                    Console.WriteLine("Error: operation (? comparison, + summation, - subtraction) not found");
-                    break;
-            }
+            var a = new Dollar { Val = 1 };
+            var b = new Ruble { Val = 30 };
+            Console.WriteLine(a + b);
             Console.ReadKey();
         }
     }
